b36be549f8975c07f1779f5d36250a3b2f071579
jesperi.kuula <jeppe.kuula@gmail.com>
2023-02-08 14:07:42 +0200

    feat: getting username and user's hero class

--- Program.cs
+++ Program.cs
@@ -9,7 +9,17 @@
             Console.WriteLine("Welcome Hero to the adventure!");
             Console.WriteLine("What is your name?");
             string userName = Console.ReadLine();
-            Console.WriteLine("Username is: " + userName);
+            Console.WriteLine(userName+ " please tell me more about yourself");
+            Console.WriteLine("Mage (1)");
+            Console.WriteLine("Ranger (2)");
+            Console.WriteLine("Rogue (3)");
+            Console.WriteLine("Warrior (4)");
+            string heroClass = Console.ReadLine();
+            Console.WriteLine("NAME: " + userName);
+            Console.WriteLine("CLASS: " + heroClass);
+            Console.WriteLine("LEVEL: 1");
+
+
         }
     }
 }
