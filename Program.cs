efe174ce172e6b6943be4daf63ad5bd1d58be651
jesperi.kuula <jeppe.kuula@gmail.com>
2023-02-08 14:28:58 +0200

    fix: github testing

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace RPGHeroes
+{
+
+
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Hero to the adventure!");
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine(userName + " please tell me more about yourself");
+            Console.WriteLine("Mage (1)");
+            Console.WriteLine("Ranger (2)");
+            Console.WriteLine("Rogue (3)");
+            Console.WriteLine("Warrior (4)");
+            string heroClass = Console.ReadLine();
+            Console.WriteLine("NAME: " + userName);
+            Console.WriteLine("CLASS: " + heroClass);
+            Console.WriteLine("LEVEL: 1");
+
+
+        }
+    }
+}
