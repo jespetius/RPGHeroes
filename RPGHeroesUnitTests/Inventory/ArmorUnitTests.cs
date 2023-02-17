3fb369fbecea8825630775b63025f6e97f4090ee
jesperi.kuula <jeppe.kuula@gmail.com>
2023-02-17 14:38:58 +0200

    feat: armor and weapon unittests

---
+++ RPGHeroesUnitTests/Inventory/ArmorUnitTests.cs
@@ -0,0 +1,41 @@
+﻿using RPGHeroes;
+using RPGHeroes.Inventory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGHeroesUnitTests.Inventory
+{
+    public class ArmorUnitTests
+    {
+        [Theory]
+        [InlineData(ArmorType.Leather)]
+        [InlineData(ArmorType.Cloth)]
+        [InlineData(ArmorType.Mail)]
+        [InlineData(ArmorType.Plate)]
+
+        public void When_NewArmorIsCreatedDifferentArmorTypes(ArmorType armorType)
+        {
+            Armor armor = new Armor("test", 1, RPGHeroes.EquipmentSlot.Head, armorType, 1, 1, 1);
+
+            Assert.NotNull(armor);
+
+        }
+
+        [Theory]
+        [InlineData(EquipmentSlot.Body)]
+        [InlineData(EquipmentSlot.Head)]
+        [InlineData(EquipmentSlot.Legs)]
+
+        public void When_NewArmorIsCreatedDifferentSlots(EquipmentSlot equipment)
+        {
+            Armor armor = new Armor("test", 1, equipment, ArmorType.Leather, 1, 1, 1);
+
+            Assert.NotNull(armor);
+
+        }
+
+    }
+}
---
+++ RPGHeroesUnitTests/Inventory/WeaponUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using RPGHeroes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RPGHeroesUnitTests.Inventory
+{
+    public class WeaponUnitTests
+    {
+        [Theory]
+        [InlineData(WeaponType.Axes)]
+        [InlineData(WeaponType.Bows)]
+        [InlineData(WeaponType.Hammers)]
+        [InlineData(WeaponType.Daggers)]
+        [InlineData(WeaponType.Swords)]
+        [InlineData(WeaponType.Staffs)]
+        [InlineData(WeaponType.Wands)]
+
+        public void When_WeaponIsCreated(WeaponType weaponType)
+        {
+            string expectedName = "this one";
+            Weapon weapon = new Weapon(expectedName, 10, 1, weaponType);
+            
+            Assert.Equal(expectedName, weapon.Name);
+        }
+
+
+    }
+}
